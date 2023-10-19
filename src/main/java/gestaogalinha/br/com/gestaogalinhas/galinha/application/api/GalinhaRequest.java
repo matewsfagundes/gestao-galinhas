69e07c3eb063d274d51d68b3c8b2d7c2d006193e
Matheus Fagundes <matewsfagundes@gmail.com>
2023-10-19 19:29:57 -0300

    implementa request e response

--- src/main/java/gestaogalinha/br/com/gestaogalinhas/galinha/application/api/GalinhaRequest.java
+++ src/main/java/gestaogalinha/br/com/gestaogalinhas/galinha/application/api/GalinhaRequest.java
@@ -1,5 +1,10 @@
 package gestaogalinha.br.com.gestaogalinhas.galinha.application.api;
 
-public class GalinhaRequest {
+import java.time.LocalDate;
 
+import lombok.Value;
+@Value
+public class GalinhaRequest {
+	private String nome;
+	private LocalDate dataNascimento;
 }
--- src/main/java/gestaogalinha/br/com/gestaogalinhas/galinha/application/api/GalinhaResponse.java
+++ src/main/java/gestaogalinha/br/com/gestaogalinhas/galinha/application/api/GalinhaResponse.java
@@ -1,5 +1,10 @@
 package gestaogalinha.br.com.gestaogalinhas.galinha.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+@Value
 public class GalinhaResponse {
+	private UUID idCliente;
 
 }
