4e800e1f83ca7aa91719ff02bb763924783103e8
Matheus Fagundes <matewsfagundes@gmail.com>
2023-10-19 16:25:12 -0300

    inicia projeto

---
+++ src/main/java/gestaogalinha/br/com/gestaogalinhas/GestaoGalinhasApplication.java
@@ -0,0 +1,13 @@
+package gestaogalinha.br.com.gestaogalinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoGalinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoGalinhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestaogalinha/br/com/gestaogalinhas/GestaoGalinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package gestaogalinha.br.com.gestaogalinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoGalinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
